517508459854d25cc0c690055cab9e3b174fa2fc
Ben Kristinsson <ben@sudo.is>
2023-10-19 08:24:14 +0200

    halfway build keymap-editor-web

---
+++ .pipeline/build-keymap-editor-web.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# Run npm in a docker container to build keymap-editor-web (Adv369-Pro-KeymapEditor)
+#
+
+set -e
+shopt -s expand_aliases
+
+alias ls='ls --color=always'
+alias grep='grep --color=always'
+
+NAME="keymap-editor-web"
+SRC_PATH="Adv360-Pro-KeymapEditor"
+SRC_REPO_URL="https://github.com/KinesisCorporation/Adv360-Pro-KeymapEditor/"
+REPO_NAME=$(git rev-parse --show-toplevel | xargs basename)
+if [[ -z "${REPO_NAME}" ]]; then
+    echo "undefined: 'REPO_NAME'"
+    exit 1
+elif [[ -z "${VERSION}" ]]; then
+    VERSION=$(date -I | tr '-' '.')-SNAPSHOT
+elif [[ ! -d "./${SRC_PATH}" ]]; then
+    git clone //github.com/KinesisCorporation/Adv360-Pro-KeymapEditor/ $(git rev-parse --show-toplevel)/Adv360-Pro-KeymapEditor
+fi
+
+# the /home/node dir is explicitly owned by uid 1000, and npm wants to write to $HOME/.npm (some logs)
+# best way seems to be to bind mount 'node_modules' from the source dir (npm expects it in its pwd).
+
+CACHE_DIR="$HOME/.cache/npm-docker/builds/${REPO_NAME}/${NAME}/.npm"
+NODE_MODULES_DIR="$HOME/.cache/npm-docker/builds/${REPO_NAME}/${NAME}/node_modules"
+OUTPUT_DIR=target/${NAME}
+
+BUILD_UID=$(id -u)
+BUILD_GID=$(id -g)
+
+echo "Cleaning up..."
+if [[ -d "./${OUTPUT_DIR}" ]]; then
+    echo "removing: '${OUTPUT_DIR}'"
+    rm -r ./${OUTPUT_DIR}
+fi
+find dist/ -name "${NAME}_${VERSION}.zip" -print -delete
+
+mkdir -pv $CACHE_DIR $NODE_MODULES_DIR $OUTPUT_DIR
+
+echo
+echo "Directories mounted to the npm container to build ${NAME} (working around the container expecting to run as uid=1000"
+echo "CACHE_DIR: ${CACHE_DIR}"
+echo "NODE_MODULES_DIR: ${NODE_MODULES_DIR}"
+
+echo
+echo "Directory mounted to write the build output to:"
+echo "OUTPUT_DIR: ${OUTPUT_DIR}"
+
+echo
+echo "Running npm container as uid=${BUILD_UID}, gid=${BUILD_GID}"
+echo
+
+if [[ -t 1 ]]; then
+    # run docker container with -t if we are in a TTY
+    DOCKER_OPT_TTY="-t"
+fi
+
+# the package.json file on the SRC repo root invokes application/package.json
+# the one in the root is for running dev sever
+#
+
+(
+    # https://github.com/KinesisCorporation/Adv360-Pro-KeymapEditor/blob/master/running-locally.md
+    #
+
+    set -e
+    set -x
+    ls -1 dist/
+    docker pull node:latest
+    docker run \
+        --rm \
+        $DOCKER_OPT_TTY \
+        -w /${NAME} \
+        -e "HOME=/home/node" \
+        -v ./${SRC_PATH}:/${NAME} \
+        -v ./config:/${NAME}/config \
+        -v ./${OUTPUT_DIR}:/${OUTPUT_DIR} \
+        -v ${CACHE_DIR}:/home/node/.npm \
+        -v ${NODE_MODULES_DIR}:/${REPO_NAME}-server/web-src/node_modules \
+        -e FORCE_COLOR=1 \
+        -e TERM="xterm256-color" \
+        -e NPM_CONFIG_PREFIX=/home/node/.npm \
+        -e NODE_PATH=/home/node/.npm/node_modules \
+        -e NODE_MODULES=/home/node/.npm/node_modules \
+        -e NODE_INSTALL_PATH=/home/node/.npm/node_modules \
+        -e OUTPUT_DIR=${OUTPUT_DIR} \
+        --user "${BUILD_UID}:${BUILD_GID}" \
+        node:latest bash -c "
+            set -ex && \
+            npm ci && \
+            npm install && \
+            cd application && \
+            npm install
+            "
+
+    # npm run build (was last)
+    # npm run build  -- --minify=false --outDir=/${OUTPUT_DIR} --emptyOutDir
+    # the docs say:
+    #  npm install
+    #  npm run dev
+    # which would presumably start a dev server. lets see how this works first
+
+    ls -lah dist/
+)
+
+#(
+#    set -e
+#    pushd $OUTPUT_DIR
+#    echo
+#    echo "creating zip file from '${OUTPUT_DIR}'"
+#    zip -r ../../dist/${NAME}_${VERSION}.zip ./
+#)
+
+echo
+ls -1 dist/
--- Jenkinsfile
+++ Jenkinsfile
@@ -16,6 +16,11 @@ pipeline {
         stage('checkout') {
             steps {
                 script {
+                    sh "git remote add github https://www.github.com/benediktkr/kinesis360"
+                    sh "git pull --rebase github main"
+                    sh "git config --global color.ui true"
+                    sh "git --no-pager log HEAD^..HEAD"
+                    //sh "git push origin main"
                     env.VERSION = sh(script: "date -I", returnStdout: true).trim().replace("-",".")
                     currentBuild.description = env.VERSION
                     sh "ls --color=always -l"
@@ -33,6 +38,17 @@ pipeline {
                 sh "make"
             }
         }
+        stage('keymap-editor-web') {
+            steps {
+                script {
+                    dir("Adv360-Pro-KeymapEditor") {
+                        git "https://github.com/KinesisCorporation/Adv360-Pro-KeymapEditor"
+                    }
+                    sh ".pipeline/build-keymap-editor-web.sh"
+                }
+            }
+        }
+
     }
     post {
         success {
@@ -41,7 +57,8 @@ pipeline {
                 fingerprint: true
                 )
             withCredentials([string(credentialsId: "gitea-user-ben-full-token", variable: 'GITEA_SECRET')]) {
-                sh 'curl -s -i -f -H "Authorization: token $GITEA_SECRET" --upload-file firmware/Adv360-firmware_${VERSION}.tar.gz https://git.sudo.is/api/packages/ben/generic/kinesis360/${VERSION}/Adv360-firmware_${VERSION}.tar.gz'
+                sh 'curl -i -H "Authorization: token $GITEA_SECRET" --upload-file firmware/Adv360-firmware_${VERSION}.tar.gz https://git.sudo.is/api/packages/ben/generic/kinesis360/${VERSION}/Adv360-firmware_${VERSION}.tar.gz'
+                // add -s -f to silence and fail on errors
             }
         }
         always {
